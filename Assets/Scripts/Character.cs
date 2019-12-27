457b59a16fd46625ec760a5c15b238cd99706672
Aviv <Avivperets26@gmail.com>
2019-12-28 01:19:49 +0200

    -A new Hero Wizard has been added with animations -New Animation foldier with animation has been added -The Tiles have collusion triggers now -The Hero movement is including animations now -Bug fixed

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    protected Vector2 direction;
+
+    //protected Vector3 targetPosition;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    //Move the Player
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        AnimateMovement(direction);
+    }
+
+    public void AnimateMovement(Vector2 direction)
+    {
+        animator.SetFloat("X", direction.x);
+        animator.SetFloat("Y", direction.y);
+    }
+
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : Character
+{
+
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+        base.Update();
+    }
+
+    
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))//UP
+        {
+            direction += Vector2.up;
+           // targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        else if (Input.GetKey(KeyCode.S))//DOWN
+        {
+            direction += Vector2.down;
+        }
+        else if (Input.GetKey(KeyCode.D))//RIGHT
+        {
+            direction += Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.A))//LEFT
+        {
+            direction += Vector2.left;
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+    }
+
+}
