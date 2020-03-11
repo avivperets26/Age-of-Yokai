1fe3076f1908164470c6b01f78d8aff5800be556
Aviv <Avivperets26@gmail.com>
2020-03-11 23:33:41 +0200

    Player adjustments and methods in player classes changes.
    Player Animations adjustments

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,46 +9,69 @@ public abstract class Character : MonoBehaviour
 
     protected Vector2 direction;
 
+
+    private Rigidbody2D myRigidbody;
     //protected Vector3 targetPosition;
 
-    private Animator animator;
+    public bool IsMoving
+    {
+        get
+        {
+            return direction.x != 0 || direction.y != 0;
+        }
+    }
+
+    private Animator myAnimator;
     // Start is called before the first frame update
     protected virtual void Start()
     {
-        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    protected virtual void Update()
+    protected virtual void Update()//Update runs once per frame
+    {
+        HandleLayers();
+    }
+    private void FixedUpdate()//FixedUpdate can run once, zero, or several times per frame, depending on how many physics frames per second are set in the time settings, and how fast/slow the framerate is. fixed Update garanteed to run and if you make them heavy then your framerate will drop. you can easily test it
     {
         Move();
     }
-
     //Move the Player
     public void Move()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        //transform.Translate(direction * speed * Time.deltaTime);
+        myRigidbody.velocity = direction.normalized * speed;//make sure that the player moves
 
-        if(direction.x !=0 || direction.y != 0)
+    }
+    public void HandleLayers()
+    {       
+
+        if (IsMoving)
         {
-            //Animate Player movements
-            AnimateMovement(direction);
+            //Make Player Animate move
+            ActivateLayer("WallkLayer");
+
+            //Sets the animation parameter so that he faces the correct direction
+            myAnimator.SetFloat("X", direction.x);
+            myAnimator.SetFloat("Y", direction.y);
         }
         else
         {
             // Player movment Animate return to  be Idle
-            animator.SetLayerWeight(1, 0);
+            ActivateLayer("IdleLayer");
         }
-        
     }
 
-    public void AnimateMovement(Vector2 direction)
+    public void ActivateLayer(string layerName)
     {
-        //Make Player Animate move
-        animator.SetLayerWeight(1, 1);
+        for (int i = 0; i < myAnimator.layerCount; i++)
+        {
+            myAnimator.SetLayerWeight(i, 0);
+        }
+        myAnimator.SetLayerWeight(myAnimator.GetLayerIndex(layerName), 1);
 
-        animator.SetFloat("X", direction.x);
-        animator.SetFloat("Y", direction.y);
     }
 
 }
