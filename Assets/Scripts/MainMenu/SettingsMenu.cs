bfe3f265d7ecdf112be1626cfa7948bc90bb2cb6
ilanY <ilanydg@gmail.com>
2020-03-19 02:52:55 +0200

    Main Menu - UPDATE #3
    in the settings menu:
    *Added volume + master mixer
    *Added Graphics dropdown that is linked to unity quality in the project settings

---
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int quilityIndex)
+    {
+        QualitySettings.SetQualityLevel(quilityIndex);
+    }
+}
