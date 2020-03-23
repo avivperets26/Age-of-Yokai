3a0901bc44b6651add7b5978c5470048f71a1789
ilanY <ilanydg@gmail.com>
2020-03-23 14:36:25 +0200

    Main Menu UPDATE #4
    Settings done
    Resolution, Full screen, Graphics

--- Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
 
     public void SetVolume(float volume)
     {
@@ -15,5 +38,17 @@ public class SettingsMenu : MonoBehaviour
     public void SetQuality(int quilityIndex)
     {
         QualitySettings.SetQualityLevel(quilityIndex);
+    }
+
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height,Screen.fullScreen);
     }
 }
