06751a8ca06067dbe588e6770f635c25b7a8e31e
ilanY <ilanydg@gmail.com>
2020-03-28 18:50:29 +0300

    Play Game Scene UPDATE#3
    some UI updates

---
+++ Assets/Scripts/Play Game/PlayGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayGame : MonoBehaviour
+{
+    public TMP_Dropdown ChooseHeroDropdown;
+ 
+    //When Cancelation warning apears and player chooses "yes" -> Go to Main Menu
+    public void Cancel()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    
+}
