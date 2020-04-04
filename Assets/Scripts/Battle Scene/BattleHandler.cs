500dbff17e00dd594d1cfb2e1359d254ba24d481
ilanY <ilanydg@gmail.com>
2020-04-04 19:52:48 +0300

    Some grid assets

---
+++ Assets/Scripts/Battle Scene/BattleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHandler : MonoBehaviour
+{
+    [SerializeField] private Transform HeroCharacter;
+
+    private void Start()
+    {
+        SpawnCharacter(true);
+        SpawnCharacter(false);
+    }
+
+    private void SpawnCharacter(bool isPlayerTeam)
+    {
+        Vector3 position;
+        if (isPlayerTeam)
+        {
+            position = new Vector3(-5, 0);
+        }
+        else
+        {
+            position = new Vector3(+5, 0);
+        }
+        Instantiate(HeroCharacter, position, Quaternion.identity);
+    }
+}
