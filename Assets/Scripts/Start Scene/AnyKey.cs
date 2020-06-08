97d74c9c65266db7f70a76336cc2b62a6d5dc78f
ilanY <ilanydg@gmail.com>
2020-06-08 19:05:23 +0300

    Start Scene

---
+++ Assets/Scripts/Start Scene/AnyKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AnyKey : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
